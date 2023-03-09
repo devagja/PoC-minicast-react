b83d28959ff705b5600f595725b1c4778e5e973f
Jose Aguilar <joshxyev@gmail.com>
2023-03-09 23:30:21 +0100

    build: add lintconf, tsconf and prettierconf

---
+++ .eslintrc.cjs
@@ -0,0 +1,81 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:import/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'standard-with-typescript'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    indent: 'off',
+    '@typescript-eslint/promise-function-async': 'off',
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/space-before-function-paren': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-this-alias': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'prefer-spread': 'off',
+    'no-case-declarations': 'off',
+    'no-console': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+    '@typescript-eslint/consistent-type-imports': 'warn',
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        groups: [
+          ['builtin'],
+          ['external'],
+          ['parent', 'internal', 'sibling', 'index', 'unknown']
+        ],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true
+        }
+      }
+    ]
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src']
+      },
+      typescript: {
+        node: {
+          extensions: ['.js', '.jsx', '.ts', '.tsx']
+        },
+        project: ['tsconfig.json']
+      }
+    },
+    react: {
+      version: 'detect'
+    }
+  }
+}
--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,25 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      // plugins: [
+      // ['@swc-jotai/debug-label', {}],
+      // ['@swc-jotai/react-refresh', {}]
+      // ]
+    }),
+    tsconfigPaths()
+  ],
   test: {
     environment: 'jsdom'
+  },
+  preview: {
+    headers: {
+      'Cache-Control': 'public,  max-age=604800, stale-while-revalidate=86400'
+    }
   }
 })
