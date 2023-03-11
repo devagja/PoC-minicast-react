901dd2809056767c1fb012824ab0e6cdd23b5f42
Jose Aguilar <joshxyev@gmail.com>
2023-03-11 10:23:39 +0100

    feat(ui): add badge component

---
+++ src/components/ui/atoms/Badge.tsx
@@ -0,0 +1,25 @@
+import type { ReactElement, ReactNode } from 'react'
+
+interface BadgeProps {
+  children: ReactNode
+  className?: string
+  [x: string]: any
+}
+
+function Badge ({
+  children,
+  className = '',
+  ...props
+}: BadgeProps): ReactElement {
+  const classes = [
+    className,
+    'badge badge-lg badge-primary rounded-lg text-xl px-2 py-1 font-bold'
+  ].join(' ')
+  return (
+    <div {...props} className={classes}>
+      {children}
+    </div>
+  )
+}
+
+export default Badge
