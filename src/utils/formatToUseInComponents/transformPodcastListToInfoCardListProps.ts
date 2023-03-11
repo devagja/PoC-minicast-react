880983a44b9169673e9f87d71961cbe71de0bf88
Jose Aguilar <joshxyev@gmail.com>
2023-03-11 10:41:32 +0100

    feat(utils): add podcastlist formatter to consume in jsx

---
+++ src/utils/formatToUseInComponents/transformPodcastListToInfoCardListProps.ts
@@ -0,0 +1,26 @@
+import type { ImgHTMLAttributes } from 'react'
+
+import type { Entry } from '~/interfaces/PodcastList'
+
+export interface InfoCardListDataProperties {
+  id: string
+  image: ImgHTMLAttributes<HTMLImageElement>
+  title: string
+  author: string
+}
+
+function transformPodcastListToInfoCardListProps (
+  podcastList: Entry[]
+): InfoCardListDataProperties[] {
+  return podcastList.map(({ id, title, imArtist, imImage, imName }) => ({
+    id: id.attributes.imId,
+    image: {
+      src: imImage[2].label,
+      alt: imName.label
+    },
+    title: title.label,
+    author: imArtist.label
+  }))
+}
+
+export default transformPodcastListToInfoCardListProps
