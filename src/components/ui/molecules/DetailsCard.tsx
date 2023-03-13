ab5dcc51497751fb51bcb73e5e7d7d9e80adda10
Jose Aguilar <joshxyev@gmail.com>
2023-03-13 22:44:14 +0100

    refactor(ui): :recycle: change detail card description prop and add loader

--- src/components/ui/molecules/DetailsCard.tsx
+++ src/components/ui/molecules/DetailsCard.tsx
@@ -4,14 +4,14 @@ interface InfoCardProps {
   image?: ImgHTMLAttributes<HTMLImageElement>
   title?: string
   author?: string
-  description?: string
+  descriptionInnerHTML?: string
 }
 
-function DetailsCard ({
+function DetailsCard({
   image = {},
   title = '',
   author = '',
-  description = ''
+  descriptionInnerHTML = ''
 }: InfoCardProps): ReactElement {
   return (
     <div className='w-full  md:max-w-[288px] flex flex-col px-3 pt-5 h-fit md:sticky top-8 bg-base-100 drop-shadow-lg border border-base-200 gap-11 pb-16'>
@@ -26,8 +26,11 @@ function DetailsCard ({
       </div>
       <div className='flex flex-col gap-1'>
         <span className='font-bold text-sm'>Description</span>
-        {description !== '' ? (
-          <span className='italic text-sm transition-all'>{description}</span>
+        {descriptionInnerHTML !== '' ? (
+          <span
+            className='italic text-sm transition-all'
+            dangerouslySetInnerHTML={{ __html: descriptionInnerHTML }}
+          />
         ) : (
           <span className='btn  btn-ghost loading btn-lg transition-all'></span>
         )}
--- src/pages/PodcastPage.tsx
+++ src/pages/PodcastPage.tsx
@@ -1,5 +1,5 @@
 import dayjs from 'dayjs'
-import { type ReactElement } from 'react'
+import { useMemo, type ReactElement } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 
 import ContainerTransition from '~/components/routing/atoms/ContainerTransition'
@@ -10,7 +10,7 @@ import usePodcastInfo from '~/hooks/query/usePodcastInfo'
 
 const intl = new Intl.DateTimeFormat('en-US')
 
-function PodcastPage(): ReactElement {
+function PodcastPage (): ReactElement {
   const nav = useNavigate()
   const params = useParams<{
     podcastId: string | undefined
@@ -23,23 +23,30 @@ function PodcastPage(): ReactElement {
     podcast?.feedUrl ?? ''
   )
 
+  console.log(podcastInfo)
   const handleSelected = (guid: string) => () => {
     nav(`episode/${guid}`)
   }
 
+  const podcastInfoLength = useMemo(
+    () => podcastInfo?.item.length,
+    [podcastInfo]
+  )
   return (
-    <ContainerTransition className='container w-full flex flex-col items-center md:items-start md:flex-row gap-3 lg:gap-24 mx-auto pt-8 pb-9 px-4'>
+    <ContainerTransition className='container mx-auto flex w-full flex-col items-center gap-3 px-4 pt-8 pb-9 md:flex-row md:items-start lg:gap-24'>
       <DetailsCard
         image={{ src: podcast?.artworkUrl600, alt: podcast?.collectionName }}
         title={podcast?.collectionName}
         author={podcast?.artistName}
-        description={podcastInfo?.itunesSummary}
+        descriptionInnerHTML={podcastInfo?.description}
       />
-      <div className='w-full flex flex-col gap-5'>
-        <div className='px-4 py-3 w-full bg-base-100 drop-shadow-lg border border-base-200  '>
-          <span className=' text-xl font-bold'>Episodes: 66</span>
+      <div className='flex w-full flex-col gap-5'>
+        <div className='w-full border border-base-200 bg-base-100 px-4 py-3 drop-shadow-lg  '>
+          <span className=' text-xl font-bold'>
+            Episodes: {podcastInfoLength}
+          </span>
         </div>
-        <div className=' bg-base-100 drop-shadow-lg border border-base-200 p-5'>
+        <div className=' border border-base-200 bg-base-100 p-5 drop-shadow-lg'>
           <InfoTable headers={['Title', 'Date', 'Duration']}>
             <tbody>
               {podcastInfo?.item.map(episode => (
@@ -47,9 +54,13 @@ function PodcastPage(): ReactElement {
                   key={episode.title}
                   title={episode.title}
                   className='cursor-pointer'
-                  onClick={handleSelected(episode.guid.text)}
+                  onClick={handleSelected(
+                    typeof episode.guid === 'object'
+                      ? episode.guid.text
+                      : episode.guid
+                  )}
                 >
-                  <td className='max-w-[15rem] text-ellipsis overflow-x-hidden lg:max-w-xs'>
+                  <td className='max-w-[15rem] overflow-x-hidden text-ellipsis lg:max-w-xs'>
                     {episode.title}
                   </td>
                   <td>{intl.format(Date.parse(episode?.pubDate))}</td>
