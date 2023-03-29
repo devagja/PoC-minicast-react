58af4484517fabe8ec1641d2bf1b18063ae0d8df
Jose Aguilar <joshxyev@gmail.com>
2023-03-29 16:06:55 +0200

    test: add searchInput unit test

--- src/components/atoms/SearchInput.tsx
+++ src/components/atoms/SearchInput.tsx
@@ -1,13 +1,13 @@
 import { forwardRef, useMemo } from 'react'
+import XmarkIcon from '../icons/XmarkIcon'
 
 // eslint-disable-next-line @typescript-eslint/consistent-indexed-object-style
 export interface SearchInputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
-  onReset: () => void
-  [x: string]: any
+  onReset?: () => void
 }
 
-const SearchInput = forwardRef(function SearchInput(
+const SearchInput = forwardRef(function _(
   { onReset, ...props }: SearchInputProps,
   ref: React.ForwardedRef<HTMLInputElement>
 ): React.ReactElement {
@@ -17,15 +17,7 @@ const SearchInput = forwardRef(function SearchInput(
         onClick={onReset}
         className='btn-primary btn-ghost absolute right-0 top-2/4 flex h-5 min-h-0 w-5 -translate-y-2/4 -translate-x-2/4 items-center justify-center rounded-md p-0'
       >
-        <svg
-          className='h-4 w-4'
-          xmlns='http://www.w3.org/2000/svg'
-          width='32'
-          height='32'
-          viewBox='0 0 512 512'
-        >
-          <polygon points='400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49' />
-        </svg>
+        <XmarkIcon />
       </button>
     ),
     [onReset]
